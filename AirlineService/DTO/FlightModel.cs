acb4041574912d6bb92a1dcc7a1fe2a58ed8417e
Evgeniya Malinina <evgeniyamalinina0707@gmail.com>
2023-10-07 21:32:39 +0300

    Added DTOs for flight and schedule

---
+++ AirlineService/DTO/FlightModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AirlineService.DTO
+{
+    public class FlightModel
+    {
+        public int ScheduleId { get; set; }
+        public DateTime Date { get; set; }
+        public int PlaneId { get; set; }
+        public string Type { get; set; } = null!;
+        public string Status { get; set; } = null!;
+        public int Gate { get; set; }
+        
+    }
+}
---
+++ AirlineService/DTO/ScheduleModel.cs
@@ -0,0 +1,13 @@
+﻿namespace AirlineService.DTO;
+
+public class ScheduleModel
+{
+    public int AirlineId { get; set; }
+    public int Number { get; set; }
+    public int DepartureAirportId { get; set; }
+    public int ArrivalAirportId { get; set; }
+    public TimeSpan    DepartureTime { get; set; }
+    public TimeSpan    ArrivalTime { get; set; }
+    public TimeSpan    FlightDuration { get; set; }
+    public int Terminal { get; set; }
+}
