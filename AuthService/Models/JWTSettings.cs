335a1ae5d71f43889acb20e518d164d5af154fbe
Evgeniya Malinina <evgeniyamalinina0707@gmail.com>
2023-10-18 21:05:18 +0300

    jwt settings

---
+++ AuthService/Models/JWTSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthService.Models;
+
+public class JWTSettings
+{
+    public string Key { get; set; }
+    public string Issuer { get; set; }
+    public string Audience { get; set; }
+}
