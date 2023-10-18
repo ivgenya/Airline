8ddfaf8da317998a5a283f3cb1c71725a93c6037
Evgeniya Malinina <evgeniyamalinina0707@gmail.com>
2023-10-18 21:02:16 +0300

    Created login and register models

---
+++ AuthService/DTO/RegisterModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthService.DTO;
+
+public class RegisterModel
+{
+    [Required]
+    public string UserName { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [Required]
+    [Compare("Password", ErrorMessage = "Passwords do not match.")]
+    [DataType(DataType.Password)]
+    public string ConfirmPassword { get; set; }
+    
+    public string[] Roles { get; set; }
+}
+    
