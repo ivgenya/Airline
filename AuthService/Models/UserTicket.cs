8621e450a703d3e0e01f7f109362fa63ec8b470c
Evgeniya Malinina <evgeniyamalinina0707@gmail.com>
2023-10-18 21:03:15 +0300

    added model of user's ticket entity

---
+++ AuthService/Models/UserTicket.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthService.Models;
+
+public class UserTicket
+{
+    public int Id { get; set; }
+    public string UserId { get; set; }
+    public ApplicationUser User { get; set; }
+}
