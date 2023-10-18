37df1a1164f775201fc432fad6515bf9bb161e31
Evgeniya Malinina <evgeniyamalinina0707@gmail.com>
2023-10-18 21:01:47 +0300

    Added applicationuser and dbcontext

---
+++ AuthService/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using AirlineService.Models;
+using AuthService.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthService.Data;
+
+public class ApplicationDbContext: IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<ApplicationUser>()
+            .HasMany(user => user.tickets)
+            .WithOne(UserTicket => UserTicket.User)
+            .HasForeignKey(ticket => ticket.UserId);
+    }
+}
+
---
+++ AuthService/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using AirlineService.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace AuthService.Models;
+
+public class ApplicationUser: IdentityUser
+{
+    public ICollection<UserTicket> tickets { get; set; }
+}
