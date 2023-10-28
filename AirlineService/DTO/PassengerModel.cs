a6ded1248968b6e2b3893b09b9dac9d9b6f1dd67
Evgeniya Malinina <evgeniyamalinina0707@gmail.com>
2023-10-29 00:41:18 +0300

    Created new models

---
+++ AirlineService/DTO/PassengerModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AirlineService.DTO;
+
+public class PassengerModel
+{
+    [Required]
+    public string Surname { get; set; } = null!;
+    [Required]
+    public string Name { get; set; } = null!;
+    [Required]
+    public string DocumentNumber { get; set; } = null!;
+    [Required]
+    public DateTime DateOfBirth { get; set; }
+    [Required]
+    public string? Gender { get; set; }
+    [Required]
+    public string Email { get; set; } = null!;
+}
---
+++ AirlineService/DTO/PaymentModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AirlineService.DTO;
+
+public class PaymentModel
+{
+    public string CreditCardNumber { get; set; }
+    public string ExpiryDate { get; set; }
+    public string Cvc { get; set; }
+}
