d87e2314df504000893c82c4d14e3ef2022ea9cc
Evgeniya Malinina <evgeniyamalinina0707@gmail.com>
2023-11-16 21:55:48 +0300

    Created UserModel

---
+++ AuthService/DTO/UserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthService.DTO;
+
+public class UserModel
+{
+    public string Id { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string Role { get; set; }
+}
