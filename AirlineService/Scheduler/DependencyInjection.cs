0d4f17efd09e2b34346bb484d7c556acf4f1c858
Evgeniya Malinina <evgeniyamalinina0707@gmail.com>
2023-11-16 21:46:20 +0300

    Added scheduler

---
+++ AirlineService/Scheduler/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using AirlineService.Services;
+using Quartz;
+
+namespace AirlineService.Scheduler;
+
+public static class DependencyInjection
+{
+    public static void AddScheduler(this IServiceCollection services) 
+    {
+        services.AddQuartz(options =>
+        {
+            options.UseMicrosoftDependencyInjectionJobFactory();
+
+            var jobKey = JobKey.Create(nameof(CheckTicketsAndBookingsState));
+            options
+                .AddJob<CheckTicketsAndBookingsState>(jobKey)
+                .AddTrigger(trigger =>
+                    trigger
+                        .ForJob(jobKey)
+                        .WithSimpleSchedule(schedule => schedule.WithIntervalInSeconds(300).RepeatForever()));
+        });
+
+        services.AddQuartzHostedService(options =>
+        {
+            options.WaitForJobsToComplete = true;
+        });
+    }
+    
+}
---
+++ AirlineService/Services/CheckTicketsAndBookingsState.cs
@@ -0,0 +1,52 @@
+﻿using AirlineService.Models;
+using AirlineService.Repositories;
+using Quartz;
+using Microsoft.Extensions.Logging;
+
+namespace AirlineService.Services;
+[DisallowConcurrentExecution]
+public class CheckTicketsAndBookingsState : IJob
+{
+    private readonly ILogger<CheckTicketsAndBookingsState> _logger;
+    private readonly IFlightRepository _flightRepository;
+    private readonly ITicketRepository _ticketRepository;
+
+    public CheckTicketsAndBookingsState(ILogger<CheckTicketsAndBookingsState> logger, IFlightRepository flightRepository, ITicketRepository ticketRepository)
+    {
+        _logger = logger;
+        _flightRepository = flightRepository;
+        _ticketRepository = ticketRepository;
+    }
+
+    public async Task Execute(IJobExecutionContext context)
+    {
+        try
+        {
+            var flightsToCheck = await _flightRepository.GetFlightsToCheck();
+            foreach (var flight in flightsToCheck)
+            {
+                var tickets = await _ticketRepository.GetAllTicketsByFlightIdAsync(flight.Id);
+                foreach (var ticket in tickets)
+                {
+                    if (ticket.Status.Equals("paid"))
+                    {
+                        ticket.State.Expire(ticket);
+                        await _ticketRepository.UpdateTicketAsync(ticket);
+                        var booking = await _ticketRepository.GetBookingByIdAsync(ticket.Id);
+                        if (booking != null)
+                        {
+                            booking.State.Complete(booking);
+                            await _ticketRepository.UpdateBookingAsync(booking);
+                        }
+                    }
+                }
+            }
+
+            _logger.LogInformation("[INFO] {Now} Booking's and ticket's states updated", DateTime.Now);
+        }
+        catch (InvalidOperationException ex)
+        {
+            _logger.LogInformation("[ERROR] {Now} An error occured while executing update", DateTime.Now);
+        }
+    }
+}
